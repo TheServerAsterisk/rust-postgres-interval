ee5dc2f915637b0a8d49d525b7a4bdc0596d3a9a
rpiper <rpiper@DESKTOP-RUBM00G.gateway.sasktel.net>
2017-09-07 21:36:05 -0600

    feat(lib): add pginterval for a 1 to 1 mapping between rust and postgres
    Ideally this type should only be used to convert from and to what the postgres protocol requires

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+#[derive(Debug)]
+/// The wire format that postgres sends intervals in. Shouldn't be used directly.
+pub struct PgInterval {
+  months: i32
+  days: i32,
+  microseconds: i64,
+}
+
+impl PgInterval {
+    /// Construct a new instance of a PgInterval.
+    pub fn new(months: i32, days: i32, microseconds: i64) -> PgInterval {
+        PgInterval {
+            months: months,
+            days: days,
+            microseconds: days
+        }
     }
 }
